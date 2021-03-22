a5bb6145b353d5d1b06f58c7cff1d8508417e7ed
Baiqi Jiang <baiqi@bu.edu>
2021-03-22 10:18:08 +0800

    Quotes app: added DetailView to show one quote.

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,8 +1,12 @@
 # quotes/urls.py
 
 from django.urls import path
-from .views import HomePageView # our view class definition
+from .views import HomePageView, QuotePageView, RandomPageView # our view class definition
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home_page"),
+    
+    path("", RandomPageView.as_view(), name="random"),
+    path("all", HomePageView.as_view(), name="all_quotes"),
+    path("quote/<int:pk>", QuotePageView.as_view(), name="quote"),
+
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Quote
+import random
+
 # Create your views here.
 
 class HomePageView(ListView):
@@ -8,3 +10,24 @@ class HomePageView(ListView):
     model = Quote # retrieve Quote objects from the database
     template_name = "quotes/home.html"
     context_object_name = "quotes"
+
+class QuotePageView(DetailView):
+    '''Display a single quote obeject.'''
+    model = Quote                           # retrieve Quote objects from the database
+    template_name = "quotes/quote.html"      # delagate the display to this template
+    context_object_name = "quote"           # use this variable name in the template
+
+class RandomPageView(DetailView):
+    '''Display a single quote obeject.'''
+    model = Quote                           # retrieve Quote objects from the database
+    template_name = "quotes/quote.html"      # delagate the display to this template
+    context_object_name = "quote"           # use this variable name in the template
+
+    def get_object(self):
+        ''' Select one quote at random for display in the quote.html template. '''
+
+        # obtain all quotes the object manager
+        quotes = Quote.objects.all()
+        # select one at random
+        q = random.choice(quotes)
+        return q
