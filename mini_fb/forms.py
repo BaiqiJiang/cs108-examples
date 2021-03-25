e94f519eb671f60644cfe2ce013fe77c44311923
Baiqi Jiang <baiqi@bu.edu>
2021-03-26 07:01:01 +0800

    added post status message feature

---
+++ mini_fb/forms.py
@@ -0,0 +1,50 @@
+# mini_fb/forms.py
+
+from django import forms
+from .models import Profile, StatusMessage
+
+class CreateProfileForm(forms.ModelForm):
+    '''A form to create a new Profile object.'''
+   
+    first_name = forms.CharField(label="First Name", required=True)
+    last_name = forms.CharField(label="Last Name", required=True)
+    birth_date = forms.CharField(label="Birth Date", required=True)
+    city = forms.CharField(label="City", required=True)
+    email_address = forms.CharField(label="Email", required=True)
+    image_url = forms.URLField(label='Image Url', required=True)
+
+    class Meta:
+        '''additional data about this form'''
+        
+        model = Profile
+        fields = ['first_name', 'last_name', 'birth_date', 'city', 'email_address', 'image_url']
+
+class UpdateProfileForm(forms.ModelForm):
+    '''A form to update an exist Profile object.'''
+   
+    first_name = forms.CharField(label="First Name", required=True)
+    last_name = forms.CharField(label="Last Name", required=True)
+    birth_date = forms.CharField(label="Birth Date", required=True)
+    city = forms.CharField(label="City", required=True)
+    email_address = forms.CharField(label="Email", required=True)
+    image_url = forms.URLField(label='Image Url', required=True)
+
+    class Meta:
+        '''additional data about this form'''
+        
+        model = Profile
+        fields = ['first_name', 'last_name', 'birth_date', 'city', 'email_address', 'image_url']
+
+class CreateStatusMessageForm(forms.ModelForm):
+    '''Allow the user input status message.'''
+
+    timestamp = forms.TimeField(required=True)
+
+    class Meta:
+        '''additional data about this form'''
+        
+        model = StatusMessage
+        fields = ['timestamp','message']
+
+
+
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,18 +11,25 @@ class Profile(models.Model):
     last_name = models.TextField(blank=True)
     city = models.TextField(blank=True)
     email_address = models.TextField(blank=True)
+    birth_date = models.TextField(blank=True)
     image_url = models.URLField(blank=True)
 
     def __str__(self):
         '''Return a string representation of these data attributes.'''
 
-        return f'{self.first_name}' + ' ' + f'{self.last_name}' + ', ' + f'{self.city}' + ', ' + f'{self.email_address}'
+        return f'{self.first_name}' + ' ' + f'{self.last_name}' + ', ' + f'{self.city}' + ', ' f'{self.birth_date}'+ ', ' + f'{self.email_address}'
     
     def get_status_messages(self):
         '''Obtain status messages for this Profile.'''
 
         # use the object manager to filter Status by this users's pk:
         return StatusMessage.objects.filter(profile=self)
+
+    def get_absolute_url(self):
+        '''Provide a url to show this object.'''
+
+        # "profile/<int:pk>"
+        return reverse('show_profile_page', kwargs={'pk':self.pk})
 
 
 class StatusMessage(models.Model):
